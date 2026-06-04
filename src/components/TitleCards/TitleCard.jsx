97522ca9253db2ee51fec1ca22164a3054fd79ed
Rayyan-sheikh2748 <rayy3748@gmail.com>
2026-06-04 23:30:30 +0530

    created structure and nevbar of netflix clone

---
+++ src/components/TitleCards/TitleCard.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./TitleCard.css";
+
+const TitleCard = () => {
+  return (
+    <div className="title-card">
+        <h1>Title Card</h1>
+    </div>
+  );
+};
+
+export default TitleCard;
